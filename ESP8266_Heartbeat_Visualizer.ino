ccbe5d078a5903a5b7ef97dbc3c4a244e5f748d8
軟實力精進聯盟 Soft Skills Union <softskillsunion@gmail.com>
2021-04-11 05:01:09 +0800

    Blynk

--- ESP8266_Heartbeat_Visualizer.ino
+++ ESP8266_Heartbeat_Visualizer.ino
@@ -2,6 +2,18 @@
 #include "MAX30100_PulseOximeter.h"
 #include "Servo.h"
 #include "Adafruit_NeoPixel.h"
+#include "BlynkSimpleEsp8266.h"
+
+/***************↓↓↓↓↓ 更改設定值 ↓↓↓↓↓***************/
+// WIFI
+char ssid[] = "SSID 名稱";
+char wifipwd[] = "SSID 密碼";
+
+// Blynk App 建立 Project 後，mail 的 auth key
+char auth[] = "Blynk 金鑰";
+/***************↑↑↑↑↑ 更改設定值 ↑↑↑↑↑***************/
+
+#define BLYNK_PRINT Serial
 
 int servoPin = 15;
 int ledPin = D5;
@@ -9,7 +21,14 @@ int ledPixels = 1;
 
 float bpm, spO2;
 
-bool isBeat = 0; // 偵測到心跳
+bool isBeat = 0;       // 偵測到心跳
+bool swVisualizer = 1; // 心跳模擬器開關
+bool swLed = 1;        // 燈號顯示開關
+
+int lastColor; // 紀錄最後燈號顏色
+
+unsigned long startBeatTime = 0; // 心跳開始時間
+int uploadInterval = 5;          // 上傳數據間隔時間(更新頻率)
 
 int posSystole = 55;  // 模擬心室收縮狀態角度
 int posDiastole = 90; // 模擬心室舒張狀態角度
@@ -21,6 +40,15 @@ Adafruit_NeoPixel pixels(ledPixels, ledPin, NEO_GRB + NEO_KHZ800);
 void setup()
 {
   Serial.begin(115200);
+
+  pixels.begin();
+
+  setLedColor(0, pixels.Color(0, 255, 255));
+
+  Blynk.begin(auth, ssid, wifipwd);
+
+  delay(500);
+
   Serial.print("Initializing pulse oximeter..");
   if (!pox.begin())
   {
@@ -40,14 +68,17 @@ void setup()
   myservo.write(posSystole);
   Serial.println("servo done");
 
-  pixels.begin();
-  pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-  pixels.show();
+  Blynk.virtualWrite(V2, swVisualizer);
+  Blynk.virtualWrite(V3, swLed);
+  Blynk.virtualWrite(V4, uploadInterval);
+
+  setLedColor(0, pixels.Color(0, 255, 0));
 }
 
 void loop()
 {
   pox.update();
+  Blynk.run();
 
   bpm = pox.getHeartRate();
   spO2 = pox.getSpO2();
@@ -56,20 +87,27 @@ void loop()
   {
     if (isBeat == 0)
     {
-      pixels.setPixelColor(0, pixels.Color(0, 0, 255));
-      pixels.show();
+      startBeatTime = millis();
+      setLedColor(0, pixels.Color(0, 0, 255));
       isBeat = 1;
+    }
+    if (millis() - startBeatTime > uploadInterval * 1000)
+    {
+      sendSensor();
+      startBeatTime = millis();
+      Serial.print("Send Data! ");
+      Serial.print(bpm);
+      Serial.print("; ");
+      Serial.println(spO2);
     }
   }
   else
   {
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    pixels.show();
+    setLedColor(0, pixels.Color(255, 0, 0));
     isBeat = 0;
     if (bpm == 0 && spO2 == 0)
     {
-      pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-      pixels.show();
+      setLedColor(0, pixels.Color(0, 255, 0));
     }
   }
 }
@@ -81,7 +119,7 @@ void onBeatDetected()
   Serial.print("; ");
   Serial.println(spO2);
 
-  if (isBeat)
+  if (swVisualizer == 1 && isBeat == 1)
   {
     if (myservo.read() == posDiastole)
     {
@@ -92,4 +130,50 @@ void onBeatDetected()
       myservo.write(posDiastole);
     }
   }
+}
+
+void setLedColor(int pixelIndex, int color)
+{
+  lastColor = color;
+  if (swLed)
+  {
+    pixels.setPixelColor(pixelIndex, color);
+    pixels.show();
+  }
+}
+
+void sendSensor()
+{
+  Blynk.virtualWrite(V0, bpm);
+  Blynk.virtualWrite(V1, spO2);
+}
+
+BLYNK_WRITE(V2)
+{
+  int x = param.asInt();
+  Serial.println(x);
+  swVisualizer = x;
+}
+
+BLYNK_WRITE(V3)
+{
+  int x = param.asInt();
+  Serial.println(x);
+  swLed = x;
+  if (x)
+  {
+    pixels.setPixelColor(0, lastColor);
+  }
+  else
+  {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  }
+  pixels.show();
+}
+
+BLYNK_WRITE(V4)
+{
+  int x = param.asInt();
+  Serial.println(x);
+  uploadInterval = x;
 }
