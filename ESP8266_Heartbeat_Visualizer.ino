b86b0c054d50e23392be41c231389f51709b69c0
軟實力精進聯盟 Soft Skills Union <softskillsunion@gmail.com>
2021-04-10 20:23:52 +0800

    SG90

--- ESP8266_Heartbeat_Visualizer.ino
+++ ESP8266_Heartbeat_Visualizer.ino
@@ -1,9 +1,17 @@
 #include "Wire.h"
 #include "MAX30100_PulseOximeter.h"
+#include "Servo.h"
 
+int servoPin = 15;
 float bpm, spO2;
 
+bool isBeat = 0; // 偵測到心跳
+
+int posSystole = 55;  // 模擬心室收縮狀態角度
+int posDiastole = 90; // 模擬心室舒張狀態角度
+
 PulseOximeter pox;
+Servo myservo;
 
 void setup()
 {
@@ -22,6 +30,10 @@ void setup()
 
   pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   pox.setOnBeatDetectedCallback(onBeatDetected);
+
+  myservo.attach(servoPin);
+  myservo.write(posSystole);
+  Serial.println("servo done");
 }
 
 void loop()
@@ -30,6 +42,18 @@ void loop()
 
   bpm = pox.getHeartRate();
   spO2 = pox.getSpO2();
+
+  if (bpm > 20 && spO2 > 40)
+  {
+    if (isBeat == 0)
+    {
+      isBeat = 1;
+    }
+  }
+  else
+  {
+    isBeat = 0;
+  }
 }
 
 void onBeatDetected()
@@ -38,4 +62,16 @@ void onBeatDetected()
   Serial.print(bpm);
   Serial.print("; ");
   Serial.println(spO2);
+
+  if (isBeat)
+  {
+    if (myservo.read() == posDiastole)
+    {
+      myservo.write(posSystole);
+    }
+    else
+    {
+      myservo.write(posDiastole);
+    }
+  }
 }
